85fe18ea33b16386f3fd18520a71d2ebb58d53cb
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-21 12:10:12 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -7,6 +7,8 @@
         'report/report_action.xml',
         'views/custom_product_label_layout_view.xml',
         'views/custom_product_label_action.xml',
+        'views/stock_picking_action_print_label.xml',  # Nueva acción de impresión
+        'views/stock_picking_view_inherit.xml',  # Botón en la vista de traslados internos
     ],
     'installable': True,
     'application': False,

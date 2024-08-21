7458ee8ce5de08281770d1ea3ec7a2779fae12ba
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-20 18:13:26 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Stock Move Label Extension',
+    'version': '1.0',
+    'depends': ['stock', 'product'],
+    'data': [
+        'views/report_action_view.xml',
+        'report/product_label_template.xml',
+    ],
+}

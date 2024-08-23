eaf4da0e23ce75bd991347de84f686195b06115f
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-22 22:37:50 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,9 @@
     'version': '1.0',
     'depends': ['stock', 'product'],
     'data': [
-        'security/ir.model.access.csv',  # Cargar primero los permisos de seguridad
-        'views/custom_product_label_action.xml'
+        'security/ir.model.access.csv',  # Permisos de seguridad
+        'report/report_product_label_template_letter.xml',  # Plantilla y reporte QWeb
+        'views/custom_product_label_action.xml'  # Vistas y acciones
     ],
     'installable': True,
     'application': False,

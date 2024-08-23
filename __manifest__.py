6c66b45ea81f863331a7c637ff821d91fbf29ab6
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-22 22:34:11 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -4,8 +4,7 @@
     'depends': ['stock', 'product'],
     'data': [
         'security/ir.model.access.csv',  # Cargar primero los permisos de seguridad
-        'report/report_product_label_template_letter.xml',  # Cargar el reporte a continuación
-        'views/custom_product_label_action.xml',  # Cargar las vistas y acciones al final
+        'views/custom_product_label_action.xml'
     ],
     'installable': True,
     'application': False,

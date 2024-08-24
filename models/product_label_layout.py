c205cd05b9c6837d17c89e44856232934f240abb
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-24 01:45:22 -0600

    Up

--- models/product_label_layout.py
+++ models/product_label_layout.py
@@ -1,4 +1,7 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class CustomProductLabelLayout(models.TransientModel):
     _name = 'custom.product.label.layout'
@@ -10,9 +13,18 @@ class CustomProductLabelLayout(models.TransientModel):
     
     def process(self):
         report_xml_id = 'stock_move_label_extension.action_report_product_label_letter'
+        
         # Verifica si el reporte existe
+        _logger.info('Attempting to generate report with ID: %s', report_xml_id)
         report = self.env.ref(report_xml_id, raise_if_not_found=False)
+        
         if report:
-            return report.report_action(self)
+            _logger.info('Report found: %s', report_xml_id)
+            try:
+                return report.report_action(self)
+            except Exception as e:
+                _logger.error('Error generating the report: %s', str(e))
+                return {'type': 'ir.actions.act_window_close'}
         else:
+            _logger.warning('Report not found for XML ID: %s', report_xml_id)
             return {'type': 'ir.actions.act_window_close'}
